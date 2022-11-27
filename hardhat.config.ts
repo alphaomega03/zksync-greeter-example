1dbaf4b312b8ec1df10f5b5cba147d405eddfc42
Janus Manlapaz <alphaomega030894@gmail.com>
2022-11-27 09:28:47 -0500

    done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 require("@matterlabs/hardhat-zksync-deploy");
 require("@matterlabs/hardhat-zksync-solc");
+require("dotenv").config();
 
 module.exports = {
   zksolc: {
@@ -14,7 +15,7 @@ module.exports = {
   },
   zkSyncDeploy: {
     zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
-    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    ethNetwork: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`, // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
   },
   networks: {
     hardhat: {
